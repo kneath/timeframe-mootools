bd83ded6cb5b0066884c92b1e4b4cbe8b93216c3
Kyle Neath <kneath@gmail.com>
2008-10-16 22:38:00 -0700

    _buildButtons implemented

--- spec/specs.js
+++ spec/specs.js
@@ -6,8 +6,95 @@
     MIT-style license.
 */
 
+var setupHTML = function(){
+  var html = '<div id="calendar"></div>';
+  document.body.appendChild(new Element('div', {id: 'htmlWrapper', style:'display:none', html: html}))
+}
+var teardownHTML = function(){
+  $('htmlWrapper').dispose();
+}
+
 describe("Initialization", {
-  'should assert the truth': function(){
-    value_of(true).should_be(true);
+  'before each': function(){
+    setupHTML();
+  },
+  'after each': function(){
+    Timeframes = [];
+    teardownHTML();
+  },
+  
+  'should push new instances to the Timeframe object': function(){
+    var old_count = Timeframes.length;
+    Instance = new Timeframe('calendar');
+    value_of(Timeframes.length).should_be(old_count + 1);
+  },
+  
+  'options sanity check': function(){
+    Instance = new Timeframe('calendar', {months: 42});
+    value_of(Instance.options.months).should_be(42);
   }
 });
+
+describe("HTML Scaffolding with default options", {
+  'before all': function(){
+    setupHTML();
+    Instance = new Timeframe('calendar');
+  },
+  'after all': function(){
+    teardownHTML();
+  },
+  
+  'should build the proper buttons': function(){
+    var buttonList = $('calendar_menu');
+    value_of(buttonList).should_not_be(null);
+    value_of(buttonList.getElement('li a.previous')).should_not_be(null);
+    value_of(buttonList.getElement('li a.today')).should_not_be(null);
+    value_of(buttonList.getElement('li a.reset')).should_not_be(null);
+    value_of(buttonList.getElement('li a.next')).should_not_be(null);
+  }
+});
+
+describe("HTML Scaffolding with custom buttons", {
+  'before each': function(){
+    setupHTML();
+    var extraHTML = '<a href="#" id="ooglyPrevious">Previously...</a><a href="#" id="ooglyToday">Today...</a><a href="#" id="ooglyReset">Reset...</a><a href="#" id="ooglyNext">Next...</a>';
+    document.body.appendChild(new Element('div', { html:extraHTML }));
+  },
+  'after each': function(){
+    teardownHTML();
+  },
+  
+  'should build the proper buttons given ids': function(){
+    Instance = new Timeframe('calendar', {
+      previousButton: 'ooglyPrevious',
+      todayButton: 'ooglyToday',
+      resetButton: 'ooglyReset',
+      nextButton: 'ooglyNext'
+    });
+    value_of($('ooglyPrevious').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyPrevious').hasClass('previous')).should_be(true);
+    value_of($('ooglyToday').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyToday').hasClass('today')).should_be(true);
+    value_of($('ooglyReset').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyReset').hasClass('reset')).should_be(true);
+    value_of($('ooglyNext').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyNext').hasClass('next')).should_be(true);
+  },
+  
+  'should build the proper buttons given elements': function(){
+    Instance = new Timeframe('calendar', {
+      previousButton: $('ooglyPrevious'),
+      todayButton: $('ooglyToday'),
+      resetButton: $('ooglyReset'),
+      nextButton: $('ooglyNext')
+    });
+    value_of($('ooglyPrevious').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyPrevious').hasClass('previous')).should_be(true);
+    value_of($('ooglyToday').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyToday').hasClass('today')).should_be(true);
+    value_of($('ooglyReset').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyReset').hasClass('reset')).should_be(true);
+    value_of($('ooglyNext').hasClass('timeframe_button')).should_be(true);
+    value_of($('ooglyNext').hasClass('next')).should_be(true);
+  }
+})
--- src/timeframe.js
+++ src/timeframe.js
@@ -6,6 +6,65 @@
     MIT-style license.
 */
 
+var Timeframes = [];
+
 var Timeframe = new Class({
+  Implements: [Options, Events],
   
+  options: {
+    months: 2,
+    format: (typeof Date.CultureInfo == 'undefined' ? '%b %d, %Y' : Date.CultureInfo.formatPatterns.shortDate),
+    monthNames: (typeof Date.CultureInfo == 'undefined' ? 'January February March April May June July August September October November December'.split(' ') : Date.CultureInfo.monthNames),
+    dayNames: (typeof Date.CultureInfo == 'undefined' ? 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(' ') : Date.CultureInfo.dayNames),
+    weekOffset: (typeof Date.CultureInfo == 'undefined' ? 0 : Date.CultureInfo.firstDayOfWeek),
+    startField: null,
+    endField: null,
+    previousButton: null,
+    todayButton: null,
+    nextButton: null,
+    resetButton: null,
+    earliest: null,
+    latest: null,
+    maxRange: false,
+  },
+  
+  // Keeps an array of the calendar tables available
+  calendars: [],
+  
+  initialize: function(element, options){
+    // Setup & Abandon if no element
+    this.element = $(element);
+    if (this.element == null) return;
+    
+    // Initial Setup
+    this.setOptions(options);
+    Timeframes.push(this);
+    
+    this._buildButtons();
+  },
+  
+  
+  
+  _buildButtons: function(){
+    var buttons = new Hash({
+      previous: { label: '&larr;', element: this.options.previousButton },
+      today:    { label: 'T',      element: this.options.todayButton },
+      reset:    { label: 'R',      element: this.options.resetButton },
+      next:     { label: '&rarr;', element: this.options.nextButton }
+    })
+    
+    var buttonList = new Element('ul', {id: this.element.id + '_menu', className: 'timeframe_menu'});
+    buttons.each(function(value, key){
+      if (value.element != null){
+        $(value.element).addClass('timeframe_button').addClass(key)
+      }else{
+        var item = new Element('li');
+        var button = new Element('a', {'class': 'timeframe_button ' + key, href: '#', text: value.label});
+        item.appendChild(button);
+        buttonList.appendChild(item);
+      }
+    });
+    
+    buttonList.inject(this.element, 'top');
+  }
 });
