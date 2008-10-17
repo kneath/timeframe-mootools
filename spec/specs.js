a7affb2bb86dad35edeecced469add2bfbcc7b2e
Kyle Neath <kneath@gmail.com>
2008-10-16 20:45:01 -0700

    Add libraries & skeleton

---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+/*
+  Script: specs.js
+    Specs for TimeFrame
+
+  License:
+    MIT-style license.
+*/
+
+describe("Initialization", {
+  'should assert the truth': function(){
+    value_of(true).should_be(true);
+  }
+});
---
+++ src/timeframe.js
@@ -0,0 +1,11 @@
+/*
+  Script: timeframe.js
+    Click-draggable. Range-makeable. A better calendar. In MoooTools
+
+  License:
+    MIT-style license.
+*/
+
+var Timeframe = new Class({
+  
+});
